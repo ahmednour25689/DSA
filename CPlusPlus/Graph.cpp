f8faadb0a3ac9e9c45332d7eba3fbf54c30bf039
ahmednour2021 <ahmed.nour@zenbusiness.com>
2023-10-15 15:43:05 +0300

    Add Graph Class

---
+++ CPlusPlus/Graph.cpp
@@ -0,0 +1,8 @@
+//
+//  Graph.cpp
+//  CPlusPlus
+//
+//  Created by Ahmed Nour on 15/10/2023.
+//
+
+#include "Graph.hpp"
---
+++ CPlusPlus/Graph.hpp
@@ -0,0 +1,13 @@
+//
+//  Graph.hpp
+//  CPlusPlus
+//
+//  Created by Ahmed Nour on 15/10/2023.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <stdio.h>
+
+#endif /* Graph_hpp */
