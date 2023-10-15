4ac1c9359656fb372dc4919229a1ae3a03c4b7dd
ahmednour2021 <ahmed.nour@zenbusiness.com>
2023-10-15 15:38:18 +0300

    Add Doubly linked list

---
+++ CPlusPlus/DoublyLinkedList.cpp
@@ -0,0 +1,8 @@
+//
+//  DoublyLinkedList.cpp
+//  CPlusPlus
+//
+//  Created by Ahmed Nour on 15/10/2023.
+//
+
+#include "DoublyLinkedList.hpp"
---
+++ CPlusPlus/DoublyLinkedList.hpp
@@ -0,0 +1,13 @@
+//
+//  DoublyLinkedList.hpp
+//  CPlusPlus
+//
+//  Created by Ahmed Nour on 15/10/2023.
+//
+
+#ifndef DoublyLinkedList_hpp
+#define DoublyLinkedList_hpp
+
+#include <stdio.h>
+
+#endif /* DoublyLinkedList_hpp */
