e036975a3c56cb7f7959bec831a0ed7f8de57174
ahmednour2021 <ahmed.nour@zenbusiness.com>
2023-10-15 12:55:24 +0300

    Implement Stack

--- CPlusPlus/Stack.cpp
+++ CPlusPlus/Stack.cpp
@@ -6,3 +6,56 @@
 //
 
 #include "Stack.hpp"
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+Stack :: Stack(int size) {
+    capacity = size;
+    stackArr = new int[size];
+    assert(stackArr != NULL);
+    numbElements = 0;
+}
+
+bool Stack :: isEmpty() {
+    return (numbElements == 0);
+}
+
+int Stack :: getTop() {
+    return  (numbElements == 0 ? -1 : stackArr[numbElements - 1]);
+}
+
+bool Stack :: push(int value) {
+    if (numbElements < capacity) {
+        stackArr[numbElements] = value;
+        numbElements++;
+        return  true;
+    }
+    else {
+        cout << "Stack Full." << endl;
+        return false;
+    }
+}
+
+int Stack:: pop() {
+    if (numbElements == 0) {
+        cout << "Stack Empty" << endl;
+        return -1;
+    }
+    else {
+        numbElements--;
+        return  stackArr[numbElements];
+    }
+}
+int Stack :: getSize() {
+    return numbElements;
+}
+
+void Stack:: showStack() {
+    int i = 0;
+    while (i < capacity) {
+        cout << '\t' << stackArr[numbElements - 1 - i];
+        i++;
+    }
+    cout << "/n";
+}
--- CPlusPlus/Stack.hpp
+++ CPlusPlus/Stack.hpp
@@ -10,4 +10,18 @@
 
 #include <stdio.h>
 
+class Stack {
+private:
+    int* stackArr;
+    int capacity;
+    int numbElements;
+public:
+    Stack(int size);
+    bool isEmpty();
+    int getTop();
+    bool push(int value);
+    int pop();
+    int getSize();
+    void showStack();
+};
 #endif /* Stack_hpp */
